a9b6597c4945c8b136e620d30f4d9f2bb8c15f3a
dl3g0 <diegojf123@gmail.com>
2021-04-14 10:14:49 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+export interface Car {
+  vin;
+  year;
+  brand;
+  color;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tablas';
+  cars= [
+    {
+      vin:'1',
+      year:'2020',
+      brand:'2',
+      color:'negro'
+    },
+    {
+      vin:'1',
+      year:'2021',
+      brand:'3',
+      color:'blanco'
+    }
+
+
+  ];
+
+    constructor() { }
+
+    ngOnInit() {
+        
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
+import {TableModule} from 'primeng/table';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule
+    
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
